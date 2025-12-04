59d4700c8d3cbc296e242654832845a4feebf190
Nevin Alcober <85alcober@gmail.com>
2025-12-04 14:19:09 +0000

    make the recent activity very below very below

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,16 +19,6 @@ export default function Home() {
         <main className="p-4 sm:p-6 lg:p-8 space-y-6">
           <WelcomeBanner />
           <StatCards />
-          <RecentActivityHeader />
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            <div className="lg:col-span-2">
-              <LatestUpdates />
-            </div>
-            <div className="space-y-6">
-              <InventoryAlerts />
-              <PerformanceSummary />
-            </div>
-          </div>
 
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2 space-y-6">
@@ -37,6 +27,17 @@ export default function Home() {
             </div>
             <div className="space-y-6">
               {/* Other components can go here if needed */}
+            </div>
+          </div>
+          
+          <RecentActivityHeader />
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="lg:col-span-2">
+              <LatestUpdates />
+            </div>
+            <div className="space-y-6">
+              <InventoryAlerts />
+              <PerformanceSummary />
             </div>
           </div>
         </main>
